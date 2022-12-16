a88c9a77f99b75d039c2ec6cf13961a0b58dfd5f
Subi Georgiev <subi.georgiev@gmail.com>
2022-12-16 13:16:35 +0200

    add ApisServicesImpl class incl. implementation for getJason()

---
+++ src/main/java/com/proect/covid/services/ApiServicesImpl.java
@@ -0,0 +1,29 @@
+package com.proect.covid.services;
+
+import com.proect.covid.services.contracts.ApisServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class ApiServicesImpl implements ApisServices {
+
+    private static final String URL = "https://api.covid19api.com/summary";
+
+    @Autowired
+    public ApiServicesImpl() {
+    }
+
+
+    @Override
+    public String getJason() throws RestClientException {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.exchange(
+                URL, HttpMethod.GET, null, String.class);
+
+        return response.getBody();
+    }
+}
