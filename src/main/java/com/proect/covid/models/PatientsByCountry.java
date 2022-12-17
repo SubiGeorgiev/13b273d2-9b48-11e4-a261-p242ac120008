937adcee1a519a622aab548fe8c96fbd7b4abb71
Subi Georgiev <subi.georgiev@gmail.com>
2022-12-17 17:41:01 +0200

    add PatientsByCountry class (persistence entity)

---
+++ src/main/java/com/proect/covid/models/PatientsByCountry.java
@@ -0,0 +1,157 @@
+package com.proect.covid.models;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "by_country")
+public class PatientsByCountry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "key_id")
+    private String keyId;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "country_code")
+    private String countryCode;
+
+    @Column(name = "slug")
+    private String slug;
+
+    @Column(name = "new_confirmed")
+    private int newConfirmed;
+
+    @Column(name = "total_confirmed")
+    private int totalConfirmed;
+
+    @Column(name = "new_deaths")
+    private int newDeaths;
+
+    @Column(name = "total_deaths")
+    private int totalDeaths;
+
+    @Column(name = "new_recovered")
+    private int newRecovered;
+
+    @Column(name = "total_recovered")
+    private int totalRecovered;
+
+    @Column(name = "date_time")
+    private LocalDateTime dateTime;
+
+    @Column(name = "premium")
+    private String premium;
+
+    public PatientsByCountry() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+
+    public String getKeyId() {
+        return keyId;
+    }
+
+    public void setKeyId(String keyId) {
+        this.keyId = keyId;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public int getNewConfirmed() {
+        return newConfirmed;
+    }
+
+    public void setNewConfirmed(int newConfirmed) {
+        this.newConfirmed = newConfirmed;
+    }
+
+    public int getTotalConfirmed() {
+        return totalConfirmed;
+    }
+
+    public void setTotalConfirmed(int totalConfirmed) {
+        this.totalConfirmed = totalConfirmed;
+    }
+
+    public int getNewDeaths() {
+        return newDeaths;
+    }
+
+    public void setNewDeaths(int newDeaths) {
+        this.newDeaths = newDeaths;
+    }
+
+    public int getTotalDeaths() {
+        return totalDeaths;
+    }
+
+    public void setTotalDeaths(int totalDeaths) {
+        this.totalDeaths = totalDeaths;
+    }
+
+    public int getNewRecovered() {
+        return newRecovered;
+    }
+
+    public void setNewRecovered(int newRecovered) {
+        this.newRecovered = newRecovered;
+    }
+
+    public int getTotalRecovered() {
+        return totalRecovered;
+    }
+
+    public void setTotalRecovered(int totalRecovered) {
+        this.totalRecovered = totalRecovered;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getPremium() {
+        return premium;
+    }
+
+    public void setPremium(String premium) {
+        this.premium = premium;
+    }
+}
