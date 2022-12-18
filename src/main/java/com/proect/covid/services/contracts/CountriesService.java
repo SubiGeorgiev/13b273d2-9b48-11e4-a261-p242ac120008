b63ddfbfe0d8abe7787cc3ca7181f6d40dc7d823
Subi Georgiev <subi.georgiev@gmail.com>
2022-12-18 23:06:03 +0200

    change signatures to saveDayByCountry and getDayByCountry methods

--- src/main/java/com/proect/covid/services/contracts/CountriesService.java
+++ src/main/java/com/proect/covid/services/contracts/CountriesService.java
@@ -1,6 +1,9 @@
 package com.proect.covid.services.contracts;
 
+import com.proect.covid.models.Country;
+
 public interface CountriesService {
 
-    public void saveDailyDataByCountry();
+    public void saveDayByCountry();
+    public Country getDayByCountry(String code);
 }
