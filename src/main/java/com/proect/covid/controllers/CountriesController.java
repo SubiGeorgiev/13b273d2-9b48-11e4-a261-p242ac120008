266248d7b8cf0c9aea7ebec317f9a81fa823f8f1
Subi Georgiev <subi.georgiev@gmail.com>
2022-12-18 23:05:06 +0200

    modify getByCountry method

--- src/main/java/com/proect/covid/controllers/CountriesController.java
+++ src/main/java/com/proect/covid/controllers/CountriesController.java
@@ -1,12 +1,18 @@
 package com.proect.covid.controllers;
 
+import com.proect.covid.models.Country;
 import com.proect.covid.services.CountriesServiceImpl;
 import com.proect.covid.services.contracts.ApisServices;
 import org.json.JSONException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.MissingResourceException;
 
 @RestController
 @RequestMapping("/api")
@@ -20,14 +26,14 @@ public class CountriesController {
     }
 
     @GetMapping("/country/{countryCode:[A-Z]{2}}")
-    public String getByCountry(@PathVariable String countryCode){
+    public Country getByCountry(@PathVariable String countryCode) {
 
         try {
-            countriesService.saveDailyDataByCountry();
+            countriesService.saveDayByCountry();
         } catch (JSONException e){
-            return COULDN_T_LOAD_DATA_FROM_API;
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, COULDN_T_LOAD_DATA_FROM_API);
         }
 
-        return countryCode;
+        return countriesService.getDayByCountry(countryCode);
     }
 }
